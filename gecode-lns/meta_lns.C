0ccbf725370e071e67b54d92929eac640e891f01
tunnuz <tunnuz@gmail.com>
2013-10-02 18:11:07 +0200

    Imported gecode-lns (from https://bitbucket.org/tunnuz/gecode-lns), created Xcode project.

---
+++ gecode-lns/meta_lns.C
@@ -0,0 +1,212 @@
+/* -*- mode: C++; c-basic-offset: 2; indent-tabs-mode: nil -*- */
+/*
+ *  Main authors:
+ *     Luca Di Gaspero <luca.digaspero@uniud.it>
+ *     Tommaso Urli <tommaso.urli@uniud.it>
+ *
+ *  Copyright:
+ *     Luca Di Gaspero, Tommaso Urli, 2013
+ *
+ *
+ */
+
+#include "meta_lns.h"
+#include "lns_space.h"
+#include <list>
+
+using namespace std;
+
+namespace Gecode { namespace Search { namespace Meta {
+  
+  /// FIXME: to be removed
+  LNSBaseOptions* LNS::lns_options;
+  
+  Space*
+  LNS::next(void) {
+    while (true) {
+
+      // We have to distinguish at least these two cases:
+      // 1. we landed here for the first time (or because of a restart)
+      // 2. we just discovered a new best solution in the previous next() call
+      // the two cases will be distinguished by the invariant that current is NULL
+      // only in case 1 and different from NULL in case 2
+      if (current == NULL)
+      { // we landed in this function for the first time or after a restart
+        intensity = lns_options->minIntensity();
+        temperature = lns_options->SAstartTemperature();
+        idle_iterations = 0;
+        neighbors_accepted = 0;
+        current = root->clone(shared);
+        LNSAbstractSpace* _current = dynamic_cast<LNSAbstractSpace*>(current);
+        _current->initial_solution_branching(restart);
+        // The initial solution is searched with a copy of the engine that has the same
+        // stop object as the overall LNS
+        se->reset(current);  // In case of reset, the space passed is not cloned by the engine
+        Space* n = se->next();
+        if (n != NULL) {
+          if (best == NULL) // it's the very first time the function is called
+          {
+            best = n->clone(shared);
+            current = n->clone(shared);
+            return n;
+          }
+          LNSAbstractSpace* _n = dynamic_cast<LNSAbstractSpace*>(n);
+          if (_n->improving(*best, true))
+          {
+            delete best;
+            best = n->clone(shared);
+            current = n->clone(shared);
+            return n;
+          }
+          else
+            current = n;
+        }
+        else // no initial solution has been found
+          return NULL;
+      }
+      else
+      { // we landed in this function after a previous call to next or we are currently looping
+        if (idle_iterations > lns_options->maxIterationsPerIntensity())
+        {
+          if (intensity < lns_options->maxIntensity())
+            intensity++;
+          idle_iterations = 0;
+          //std::cerr << "Current intensity " << intensity << std::endl;
+        }
+        if (neighbors_accepted > lns_options->SAneighborsAccepted())
+        {
+          temperature *= lns_options->SAcoolingRate();
+          neighbors_accepted = 0;
+        }
+        Space* neighbor = root->clone(shared);
+        LNSAbstractSpace* _current = dynamic_cast<LNSAbstractSpace*>(current);
+        unsigned int relaxed_variables = _current->relax(neighbor, intensity);
+        LNSAbstractSpace* _neighbor = dynamic_cast<LNSAbstractSpace*>(neighbor);
+        _neighbor->neighborhood_branching();
+        switch (lns_options->constrainType()) {
+          case LNS_CT_LOOSE:
+            _neighbor->constrain(*current, false, 0.0);
+            break;
+          case LNS_CT_STRICT:
+            _neighbor->constrain(*current, true, 0.0);
+            break;
+          case LNS_CT_SA:
+          {
+            double p = r(RAND_MAX) / (double)RAND_MAX; // p should be a uniformly random number in (0, 1]
+            double delta = -temperature * std::log(p);
+            _neighbor->constrain(*current, false, delta);
+          }
+            break;
+          case LNS_CT_NONE:
+          default:
+            break;
+        }
+        Space* n = NULL;
+        SpaceStatus neighbor_status = neighbor->status(stats);
+        if (neighbor_status == SS_SOLVED)
+          n = neighbor;
+        else if (neighbor_status == SS_FAILED)
+        {
+          delete neighbor;
+          n = NULL;
+        }
+        else
+        {
+          e->reset(neighbor); // keep in mind that in case of reset, the Space passed to the engine is not cloned
+          e_stop->limit(relaxed_variables * lns_options->timePerVariable());
+          e_stop->reset();
+          std::list<Space*> prev_solutions;
+          do
+            prev_solutions.push_back(e->next());
+          while (prev_solutions.back() != NULL);
+          if (prev_solutions.size() > 1)
+            prev_solutions.pop_back(); // remove the last NULL solution
+          n = prev_solutions.back();
+          prev_solutions.pop_back();
+          for (std::list<Space*>::iterator it = prev_solutions.begin(); it != prev_solutions.end(); it++)
+            delete *it;
+        }
+        if (n != NULL)
+        {
+          
+          neighbors_accepted++;
+          LNSAbstractSpace* _n = dynamic_cast<LNSAbstractSpace*>(n);
+          if (_n->improving(*best, true))
+          {
+            delete best;
+            best = n->clone(shared);
+            delete current;
+            current = n->clone(shared);
+            idle_iterations = 0;
+            intensity = lns_options->minIntensity();
+            return n;
+          }
+          else if (lns_options->constrainType() == LNS_CT_SA || lns_options->constrainType() == LNS_CT_NONE || _n->improving(*current, lns_options->constrainType() == LNS_CT_STRICT))
+          {
+            delete current;
+            current = n;
+          }
+        }
+        if (m_stop != NULL && m_stop->stop(statistics(), opt)) // the overall search has to be stopped
+        {
+          // eventually ask to restart
+          if (n != NULL)
+            delete n;
+          if (neighbor != NULL)
+          {
+            //cerr << "Neighbor is not null, and it's ok to erase it." << endl;
+            //delete neighbor;
+          }
+          delete current;
+          current = NULL;
+          restart++;
+          return NULL;
+        }
+      }
+      idle_iterations++;
+    }
+    GECODE_NEVER;
+        
+    return NULL;
+  }
+  
+  Search::Statistics
+  LNS::statistics(void) const {
+    return stats + e->statistics();
+  }
+  
+  bool
+  LNS::stopped(void) const {
+    /*
+     * What might happen during parallel search is that the
+     * engine has been stopped but the meta engine has not, so
+     * the meta engine does not perform a restart. However the
+     * invocation of next will do so and no restart will be
+     * missed.
+     */
+    return e->stopped();
+  }
+  
+  void
+  LNS::reset(Space* s) {
+    current = s;
+    LNSAbstractSpace* _s = dynamic_cast<LNSAbstractSpace*>(s);
+    if (best && _s->improving(*best, true))
+    {
+      delete best;
+      best = s->clone(shared);
+    }
+    idle_iterations = 0;
+    intensity = lns_options->minIntensity();
+    neighbors_accepted = 0;
+    temperature = lns_options->SAstartTemperature();
+  }
+  
+  LNS::~LNS(void) {
+    // Deleting e also deletes stop
+    delete e;
+  }
+  
+}}}
+
+// STATISTICS: search-other
---
+++ gecode-lns/meta_lns.h
@@ -0,0 +1,86 @@
+/* -*- mode: C++; c-basic-offset: 2; indent-tabs-mode: nil -*- */
+/*
+ *  Main authors:
+ *     Luca Di Gaspero <luca.digaspero@uniud.it>
+ *     Tommaso Urli <tommaso.urli@uniud.it>
+ *
+ *  Copyright:
+ *     Luca Di Gaspero, Tommaso Urli, 2013
+ *
+ *
+ */
+
+
+
+#ifndef __GECODE_SEARCH_META_LNS_HH__
+#define __GECODE_SEARCH_META_LNS_HH__
+
+#include <gecode/search.hh>
+#include "lns.h"
+
+namespace Gecode { namespace Search { namespace Meta {
+
+  /// Engine for restart-based search
+  class LNS : public Engine {
+  private:
+    /// The actual engine(s)
+    Engine* se;
+    Engine* e;
+    /// The root space to create new partial solutions from scratch
+    Space* root;
+    /// The best solution that far
+    Space* best;
+    /// The current solution
+    Space* current;
+    /// The stop control object for the sub-engine
+    TimeStop* e_stop;
+    /// The stop control object for the overall LNS
+    Stop* m_stop;
+    /// The statistics
+    Search::Statistics& stats;
+    /// The options
+    const Options& opt;
+    /// The number of times stop has reached
+    unsigned long int restart;
+    /// The number of idle iterations performed (for detecting stagnation)
+    unsigned long int idle_iterations;
+    /// The current intensity for LNS
+    unsigned int intensity;
+    /// Whether the slave can be shared with the master
+    bool shared;
+    /// Random numbers generator
+    Rnd r;
+    /// Current temperature for SA
+    double temperature;
+    /// Neighbors accepted at current temperature
+    unsigned long int neighbors_accepted;
+  public:
+    /// Constructor
+    LNS(Space*, size_t, TimeStop* e_stop0,
+        Engine* se0, Engine* e0, Search::Statistics& stats0, const Options& opt0);
+    /// Return next solution (NULL, if none exists or search has been stopped)
+    virtual Space* next(void);
+    /// Return statistics
+    virtual Search::Statistics statistics(void) const;
+    /// Check whether engine has been stopped
+    virtual bool stopped(void) const;
+    /// Reset engine to restart at space \a s
+    virtual void reset(Space* s);
+    /// Destructor
+    virtual ~LNS(void);
+    /// FIXME: waiting for a definitive way to pass specific options to the (meta-)engines, currently they will be embedded in
+    /// a static member of the LNS class
+    static LNSBaseOptions* lns_options;
+  };
+
+  forceinline
+  LNS::LNS(Space* s, size_t, TimeStop* e_stop0, 
+           Engine* se0, Engine* e0, Search::Statistics& stats0, const Options& opt0)
+    : se(se0), e(e0), root(s), best(0), current(0), e_stop(e_stop0), m_stop(opt0.stop), stats(stats0), opt(opt0), restart(0), idle_iterations(0),
+  shared(opt.threads == 1), temperature(1.0) {}
+
+}}}
+
+#endif
+
+// STATISTICS: search-other
